496d584fdd27de795a2c0d79422d849952252fdc
Clayten Bruce <cj.bruce.clayten@gmail.com>
2021-12-16 07:59:46 -0700

    Changed img-card to fit better and removed test code at bottom

--- darkish/src/client/modules/my/imgCard/imgCard.js
+++ darkish/src/client/modules/my/imgCard/imgCard.js
@@ -1,5 +1,7 @@
-import{LightningElement} from 'lwc';
+import{LightningElement, api} from 'lwc';
 
 export default class Home extends LightningElement{
-    
+    @api avalability = 'avaliable';
+    @api price = '$0.00';
+    @api image = '';
 }
